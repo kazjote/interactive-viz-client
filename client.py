e24f422ae01138dd2012813ce0654292484b75ec
Kacper <kazjote@gmail.com>
2023-04-04 09:09:33 +0200

    Send draw command

--- client.py
+++ client.py
@@ -1,4 +1,18 @@
+# Packages: dbus-python numpy matplotlib
+
 import dbus
+import numpy as np
+import matplotlib.pyplot as plt
 
 session_bus = dbus.SessionBus()
 proxy = session_bus.get_object("eu.kazjote.InteractiveViz", '/eu/kazjote/InteractiveViz')
+interactive_viz = dbus.Interface(proxy,
+    dbus_interface='eu.kazjote.InteractiveViz')
+
+x = np.linspace(-10, 10, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.savefig('/tmp/plot.svg')
+
+interactive_viz.ReloadPicture('/tmp/plot.svg')
