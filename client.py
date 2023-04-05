85090384e2d9a938b15ebdceca8fd8dca18245c6
Kacper <kazjote@gmail.com>
2023-04-05 07:07:50 +0200

    Establish communication

--- client.py
+++ client.py
@@ -1,8 +1,13 @@
-# Packages: dbus-python numpy matplotlib
+# Packages: dbus-python numpy matplotlib PyGObject
 
 import dbus
 import numpy as np
 import matplotlib.pyplot as plt
+from dbus.mainloop.glib import DBusGMainLoop
+import time
+from gi.repository import GLib
+
+DBusGMainLoop(set_as_default=True)
 
 session_bus = dbus.SessionBus()
 proxy = session_bus.get_object("eu.kazjote.InteractiveViz", '/eu/kazjote/InteractiveViz')
@@ -16,3 +21,7 @@ plt.plot(x, y)
 plt.savefig('/tmp/plot.svg')
 
 interactive_viz.ReloadPicture('/tmp/plot.svg')
+interactive_viz.connect_to_signal('ArgumentsChanged', lambda args: print(f'Arguments changed: {args}'))
+
+loop = GLib.MainLoop()
+loop.run()
